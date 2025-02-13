87fd50b3854dd8188f34833c473af4026ba72484
SingleThreaded400 <av0232016@gmail.com>
2025-02-13 11:55:09 +0530

    Add files via upload

--- src/Hooks/useLenis.jsx
+++ src/Hooks/useLenis.jsx
@@ -1,1 +1,29 @@
+import { useEffect, useState } from "react";
+import Lenis from "@studio-freight/lenis";
+
+const useLenis = () => {
+  const [lenis, setLenis] = useState(null);
+
+  useEffect(() => {
+    const lenisInstance = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    });
+
+    setLenis(lenisInstance); // Set Lenis instance
+
+    function raf(time) {
+      lenisInstance.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    return () => lenisInstance.destroy();
+  }, []);
+
+  return lenis;
+};
+
+export default useLenis;
 
