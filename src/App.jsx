6bfc23f9197e210e08909e977d2844d3c0d330db
SingleThreaded400 <av0232016@gmail.com>
2025-10-21 21:57:32 +0530

    Replace Navbar2 with Navbar3 in App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact2 from "./components/Contact2.jsx";
 import Working from "./components/MyWorkingProcess.jsx";
 import Loading from "./components/Loading.jsx";
 //import useLenis from "./Hooks/useLenis.jsx";
-import Navbar2 from "./components/Navbar2.jsx";
+import Navbar3 from "./components/Navbar3.jsx";
 import Footer from "./components/Footer.jsx";
 //import LocomotiveScroll from 'locomotive-scroll';
 import Bharat from "./components/Bharat.jsx";
@@ -23,7 +23,7 @@ function App() {
         <Loading onComplete={() => setLoading(false)} />
       ) : (
         <>
-          <Navbar2 />
+          <Navbar3 />
           <div id="hero">
             <Hero3 />
           </div>
